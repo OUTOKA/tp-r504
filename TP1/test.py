fe29192617970293c82f7d9b48f25934e7986767
OUTOKA <antoine.didierjean05@gmail.com>
2024-09-11 11:08:31 +0200

    ll

--- TP1/test.py
+++ TP1/test.py
@@ -7,8 +7,8 @@ def test_1 ( ) :
 def test_2 ( ) :
 	assert f . puissance (2 ,3) == 8
 	assert f . puissance (2 ,2) == 4
-
-def tets_3 ( ) :
-	assert f . puissance (0.5 ,3) == 0.125
-	assert f . puissance (3 ,0.5) == 1.732050808
-	assert f . puissance (3 ,-0.5) == 0.5773502692  
+	assert f . puissance (1 ,1) == 1
+	assert f . puissance (1 ,-1) == 1
+	assert f . puissance (-1 ,1) == -1
+	assert f . puissance (-1 ,-1) == -1
+	assert f . puissance (0 ,-1) == 0
