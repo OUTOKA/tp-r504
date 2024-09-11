66843944e702469ad072050a03f76a22fb67d2f3
OUTOKA <antoine.didierjean05@gmail.com>
2024-09-11 11:27:21 +0200

    ex 5

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,6 +1,9 @@
 def puissance(a, b):
     if not type(a) is int or not type(b) is int:
         raise TypeError("Only integers are allowed")
-    return a ** b
-
+    res = 1
+    for i in range(1, b + 1):
+        res = res * a
+    
+    return res
 
