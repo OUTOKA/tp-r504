c4ec1598f70317ba15919bf6f7e666f51681b99a
OUTOKA <antoine.didierjean05@gmail.com>
2024-10-10 15:35:31 +0200

    deuxieme commit

--- TP-LB/tp-A/start1.sh
+++ TP-LB/tp-A/start1.sh
@@ -1,9 +1,3 @@
-docker create network tplb
-docker build -t im-nginx-lb .
-mkdir shared1
-mkdir shared2
-echo "<h1>Hello 1</h1>" > shared1/index.html
-echo "<h1>Hello 2</h1>" > shared2/index.html
 
 docker run --name nginx1 --rm -d -p 81:80\
    --mount type=bind,source="$(pwd)"/shared1,target=/usr/share/nginx/html \
---
+++ TP-LB/tp-A/stop.sh
@@ -0,0 +1,5 @@
+docker stop nginx1
+docker stop nginx2
+docker stop nginx-lb
+
+docker network rm tplb
