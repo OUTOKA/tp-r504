57992de187f0cadf2806b84c10834807a2b29afc
OUTOKA <antoine.didierjean05@gmail.com>
2025-03-06 11:26:30 +0100

    v1 et v2 fonctionnel

--- nmap/scan_A106_v2.sh
+++ nmap/scan_A106_v2.sh
@@ -3,7 +3,7 @@
 nmap -oG out.txt 172.16.0.0/24
 nmap -sU -F -oG out2.txt 172.16.0.0/24
 
-echo "# - IP - TCP ports" > "scan-result_2.csv"
+echo "# - IP - TCP ports - UDP ports" > "scan-result_2.csv"
 
 for ip in $( cat out.txt | grep "Ports:" | awk '{print $2}'); do
   open_ports_tcp=$(cat out.txt | grep $ip | grep -oP '\d+\/open\/tcp' | wc -l)
