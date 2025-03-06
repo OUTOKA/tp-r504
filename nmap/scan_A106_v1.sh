adbee6876defd7a75a3ebb2329bf47d672f27f91
OUTOKA <antoine.didierjean05@gmail.com>
2025-03-06 10:51:58 +0100

    fin de tp

--- nmap/scan_A106_v1.sh
+++ nmap/scan_A106_v1.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+nmap -oG out.txt 172.16.0.0/24
+
 echo "# - IP - TCP ports" > "scan-result_1.csv"
 
-for ip in $(nmap -sn 172.16.0.0/24 | grep "Nmap scan report for" | awk '{print $5}'); do
-  open_ports=$(nmap --open -T4 $ip | grep "^80/tcp" -c)
+for ip in $( cat out.txt | grep "Ports:" | awk '{print $2}'); do
+  open_ports=$(cat out.txt | grep $ip | grep -oP '\d+\/open\/tcp' | wc -l)
   
   echo "$ip;$open_ports" >> "scan-result_1.csv"
 done
 
 echo "Scan terminé. Voir scan-result_1.csv."
+
+
+
+
