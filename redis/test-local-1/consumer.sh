0ec11e0c9579a32c83ed0c1c62c29336e839f1b2
user <user@debian>
2025-04-30 09:07:57 -0400

    sh qui marche

---
+++ redis/test-local-1/consumer.sh
@@ -0,0 +1,25 @@
+
+threshold=10000
+delay=2
+
+redis-cli DBSIZE >/dev/null
+if ! [ $? = 0 ]; then
+	echo "Erreur, pas de connection avec le serveur redis!"
+	exit 1
+fi
+
+while :
+do
+	nb=$(redis-cli --raw LLEN mafile)
+	if [ "$nb" -gt 0 ]; then
+		valeur=$(redis-cli --raw RPOP mafile)
+		if [ "$nb" -gt "$threshold" ]; then
+			echo "ALARME ! valeur = $nb"
+			sleep $delay
+		fi
+	else
+		echo "Liste vide, attente 2s."
+		sleep $delay
+	fi
+done
+
